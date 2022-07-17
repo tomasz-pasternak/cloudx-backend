c30a513118d4807248b25eaddcf895976abc5cf7
Tomasz <pasternak.tomek@gmail.com>
2022-07-17 22:02:40 +0200

    Product API

---
+++ product-service/handler.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports.getProductsList = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+module.exports.getProductsById = async (event) => {
+  console.log("byid", event);
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'By id',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
